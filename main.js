646bebe660c2194774222c45aec44ab0de2bf542
Le Chevalier <assawinknw48@gmail.com>
2023-12-26 22:38:33 +0700

    release 1.0

--- main.js
+++ main.js
@@ -8,8 +8,19 @@ root.render(<App />);
 //     </div>
 // );
 
-function Counter(/*props*/) {
-    const [countNum, setCountNum] = React.useState(0);
+function Suminfo({counters}) {
+    const total = counters.reduce( (accumulator, currentValue) => 
+    accumulator + currentValue.number, 0);
+
+    return (
+        <div className="showSum">
+            <h2 id="sum" style={{color: 'lightgreen'}}>Sum = {total}</h2>
+        </div>
+    )
+}
+
+function Counter(props) {
+    //const [countNum, setCountNum] = React.useState(0);
 
     // const incCount = () => {
     //     setCountNum(countNum + 1);
@@ -27,44 +38,91 @@ function Counter(/*props*/) {
     //     setCountNum(countNum - countNum);
     // }
 
-    const updateCounter = (n) => {
-        if(countNum + n < 0) {
-            return;
-        }
-        setCountNum( (prev) => {
-            return prev + n;
-        } );  
-    }
+    //const updateCounter = (n) => {
+        // if(countNum + n < 0) {
+        //     return;
+        // }
+        // setCountNum( (prev) => {
+        //     return prev + n;
+        // } );  
+    //}
 
 
     // const styles = {
     //     color : props.color,
     //     fontSize : props.size === 'big' ? '50px' : '40px'
     // }
+
+    const {item: {id, number}, hdUpdate, hdRemCounter} = props;
+
     return (
-        <div className="showSum">
-            <h2 id="sum" style={{color: 'lightgreen'}}>Sum = 0</h2>
-            <button type="button">Add Counter</button>
-            <div className="counter">
-                <button onClick = {()=>updateCounter(1)}>+</button>
-                <h2>{countNum}</h2>
-                <button onClick = {()=>updateCounter(-1)}>-</button>
-                <button onClick = {()=>updateCounter(-countNum)}>C</button>
-                <button>X</button>
-            </div>
+        <div className="counter">
+                <button onClick = {()=>hdUpdate(id, 1)}>+</button>
+                <h2>{number}</h2>
+                <button onClick = {()=>hdUpdate(id, -1)}>-</button>
+                <button onClick = {()=>hdUpdate(id, -number)}>C</button>
+                <button onClick = {()=>hdRemCounter(id)}>X</button>
         </div>
+            // <div className="counter">
+            //     <button onClick = {()=>props.hdUpdate(props.item.id, 1)}>+</button>
+            //     <h2>{props.item.number}</h2>
+            //     <button onClick = {()=>props.hdUpdate(props.item.id, -1)}>-</button>
+            //     <button onClick = {()=>props.hdUpdate(props.item.id, -props.item.number)}>C</button>
+            //     <button>X</button>
+            // </div>
     )
 }
 
 function App() {
+    const [counters, setCounter] = React.useState([{id: 1, number: 0}])
+    // let allCounter = Array(counters).fill(<Counter key={Math.random()}/>);
+    // console.log(counters[idx].number);
+
+    const hdUpdate = (id, num) => {
+        const cloneCounters = [...counters];
+        let idx = cloneCounters.findIndex(el => el.id === id);
+        // let total = 0;
+        console.log(idx, num)
+        if(cloneCounters[idx].number + num < 0) {
+            return;
+        }
+        cloneCounters[idx].number += num;
+        // total = cloneCounters[idx].number + num;
+        // console.log(total);
+        setCounter(cloneCounters);
+    }
+
+    const hdAddCounter = () => {
+        let newId = counters.length===0 ? 1 : counters.at(-1).id + 1;
+        // let newId = counters.length===0 ? 1 : counters.slice(-1)[0].id + 1
+        // setCounter([...counters, {id: newId, number: 0}]);
+        const cloneCounters = [...counters];
+        cloneCounters.push( {id: newId, number: 0} );
+        setCounter(cloneCounters);
+    }
+
+    const hdRemCounter = (id) => {
+        const updateCounter = counters.filter(el => el.id !== id);
+        setCounter(updateCounter);
+
+        // setCounter(prevCounters => prevCounters.filter(el => el.id !== id));
+    }
+
+
+
     return (
-        <div>
+        <div id="app"> 
             <h1>Hola!</h1>
             <h2>Today : { (new Date()).toDateString() }</h2>
-            <br />
-            <hr />
-            <Counter /*color="red" size="small"*//>
+            <br /><hr />
+            <Suminfo counters={counters}/> 
+            <button onClick={ hdAddCounter }>Add Counter</button>
+            {counters.map(el => {
+                return <Counter key={el.id} item={el} hdUpdate={hdUpdate} hdRemCounter={hdRemCounter}/>
+            })}
         </div>
     );
 }
+
+// In Suminfo attribute === {attribute} such as counters = {counters}
 
