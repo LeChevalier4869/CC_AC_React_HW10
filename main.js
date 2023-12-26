65eecdf53f0e3281daef05d445d6c6f6731420d7
Le Chevalier <assawinknw48@gmail.com>
2023-12-26 11:26:31 +0700

    add function for button

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<App />);
+
 
 // const content = (
 //     <div>
@@ -6,16 +8,48 @@ const root = ReactDOM.createRoot(document.querySelector('#root'));
 //     </div>
 // );
 
-function Counter() {
+function Counter(/*props*/) {
+    const [countNum, setCountNum] = React.useState(0);
+
+    // const incCount = () => {
+    //     setCountNum(countNum + 1);
+    // }
+
+    // const decCount = () => {
+    //     if(countNum <= 0){
+    //         setCountNum(countNum - countNum);
+    //     } else {
+    //         setCountNum(countNum - 1);
+    //     }
+    // }
+
+    // const clrCount = () => {
+    //     setCountNum(countNum - countNum);
+    // }
+
+    const updateCounter = (n) => {
+        if(countNum + n < 0) {
+            return;
+        }
+        setCountNum( (prev) => {
+            return prev + n;
+        } );  
+    }
+
+
+    // const styles = {
+    //     color : props.color,
+    //     fontSize : props.size === 'big' ? '50px' : '40px'
+    // }
     return (
         <div className="showSum">
-            <h2 id="sum">Sum = 0</h2>
+            <h2 id="sum" style={{color: 'lightgreen'}}>Sum = 0</h2>
             <button type="button">Add Counter</button>
             <div className="counter">
-                <button>+</button>
-                <h2>0</h2>
-                <button>-</button>
-                <button>C</button>
+                <button onClick = {()=>updateCounter(1)}>+</button>
+                <h2>{countNum}</h2>
+                <button onClick = {()=>updateCounter(-1)}>-</button>
+                <button onClick = {()=>updateCounter(-countNum)}>C</button>
                 <button>X</button>
             </div>
         </div>
@@ -29,9 +63,8 @@ function App() {
             <h2>Today : { (new Date()).toDateString() }</h2>
             <br />
             <hr />
-            <Counter />
+            <Counter /*color="red" size="small"*//>
         </div>
     );
 }
 
-root.render(<App />);
